e06c4423c7f75a6da61d5bdbbc593612b8962bfd
Manar Rostom <121633705+ManarRostom@users.noreply.github.com>
2023-12-30 19:30:34 +0200

    Update Deployment.py

--- Deployment.py
+++ Deployment.py
@@ -7,7 +7,7 @@ import sklearn
 import category_encoders
 
 inputs_dict = joblib.load('inputs_dict.pkl')
-Model_ = joblib.load("Model.pkl")
+Model_ = joblib.load("model")
 mlb_dict = joblib.load('mlb_dict.pkl')
 
 def encode_cats(df_cats):
